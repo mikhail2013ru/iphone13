a3aa402b9dcc7f323569fa1f8f7f02fb97ace566
mikhail <mikhail2013ru@gmail.com>
2021-10-20 17:50:27 +0300

    Стилизация checkbox

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const checkbox = document.querySelectorAll('.checkbox__input');
+const formLabel = document.querySelectorAll('.form__label');
+
+// checkbox.addEventListener('click', () => {
+//     console.log('sosi');
+// });
+
+// checkbox.forEach(function(item, i) {    
+//     formLabel.forEach(function(elem, e) {
+//         elem.addEventListener('click', (e) => {
+//         console.log(e);
+//     });
+// });
